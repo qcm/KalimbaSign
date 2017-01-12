7063cc992c8a6de2f8ebfe4872c7698a980ca5e9
Bo-Yi Lee <boyil@qca.qualcomm.com>
2017-01-12 15:50:06 -0800

    Changes: 1. Finish argparse and optparse Todo: 1. Generate all the output files, completes TLVGenerator()

--- kalimba_sig.py
+++ kalimba_sig.py
@@ -44,10 +44,11 @@ TLV_RSP_CFG_NO_CC_ACK_VSE  = 2
 TLV_RSP_CFG_NO_CC_NO_VSE   = 3 
 #IMG_TYPE = int(os.environ['IMAGE_TYPE'], 10)
 IMG_TYPE = 0 # read input from optParser
-PRODUCT_ID = b'\x0d' + b'\x00' #?
+PRODUCT_ID = 0
 #ROM_BUILD_VER = int(os.environ['BUILD_VER'], 10) #? from where? and order?
 ROM_BUILD_VER = 0 # read input from optParser
-PATCH_VER = b'\x02' + b'\x00'  #?
+#PATCH_VER = b'\x02' + b'\x00'  #?
+PATCH_VER = 0
 RSV_BYTES_0 = b'\x00' + b'\x00'
 #ANT_RB_VER = os.environ['ANTIROLLBACK_VERSION'].lstrip('0x').zfill(2)
 ANT_RB_VER = 0 # read input from optParser
@@ -56,7 +57,8 @@ SERIAL_LOW = 0 # read input from optParser
 #SERIAL_HIGH = os.environ['SERIAL_NO_HIGH'].lstrip('0x').zfill(4)
 SERIAL_HIGH = 0 # read input from optParser
 RSV_BYTES_1 = b'\x00' + b'\x00'
-ENTRY_ADDR = b'\x98' + b'\x33' + b'\x02' + b'\x00'
+#ENTRY_ADDR = b'\x98' + b'\x33' + b'\x02' + b'\x00'
+ENTRY_ADDR = b'\x00' * 4
 TLV_HEADER = 36 # 36 bytes header. Please refer the doc
 CRC32 = b'\x00' * 4
 SIGNATURE = b'\x00'* 256 # 256 bytes for RSA-2048
@@ -67,27 +69,31 @@ BIN_FIN = "FILES/QCA6290_SCAQBAFM_rampatch.bin"
 BIN_SIZE = 0
 PUB_KEY_FILE = "FILES/test_key.txt"
 PRI_KEY_FILE = "FILES/test_prv_key.pem"
-DFU_FOUT = "FILES/QCA6290_SCAQBAFM_rampatch.dfu"
+SIGNED_FOUT = "QCA6290_SCAQBAFM_rampatch_signed.tlv"
+SIGNED_OPT_FOUT = "QCA6290_SCAQBAFM_rampatch_signed_opt.tlv"
+HASH_FOUT = "QCA6290_SCAQBAFM_rampatch_signed_opt.tlv"
 
 
 def optParser():
 	global SIGN_ALGO, IMG_TYPE, PRODUCT_ID, ROM_BUILD_VER, PATCH_VER
 	global ANT_RB_VER, SERIAL_LOW, SERIAL_HIGH, ENTRY_ADDR
 	desc = 'Signing Tool version' + str(SIGN_FMT_VER)
-	if sys.hexversion < PYTHON_VERSION:
-	#if sys.hexversion >= PYTHON_VERSION:
+	#if sys.hexversion < PYTHON_VERSION:
+	if sys.hexversion >= PYTHON_VERSION:
 		import argparse
+		print "using ArgumentParser"
 		parser = argparse.ArgumentParser(description = desc)
-		parser.add_argument('-a', '--ALGO', default=2, help='0:SHA256/ 1:ECDSA_P-256/ 2:RSA-2048_SHA256')
-		parser.add_argument('-b', '--ROM_VER', default=0, help='Patchee\'s rom build version')
-		parser.add_argument('-j', '--ENTRY_ADDR', default=0, help='The address of the application\'s entry')
-		parser.add_argument('-p', '--PATCH_VER', default=0, help='Patchee\'s patch build version')
-		parser.add_argument('-r', '--IMG_TYPE', default=0, help='Patch Image type')
-		parser.add_argument('-s', '--ANTI_ROLLBACK_VER', default=0, help='Image anti-rollback version')
-		parser.add_argument('-t', '--PROD_ID', default=0, help='Specify production Types/ID')
-		parser.add_argument('-x', '--SERIAL_LOW', default=0, help='Minor serial number')
-		parser.add_argument('-y', '--SERIAL_HIGH', default=0, help='Major serial number')
+		parser.add_argument('-a', '--ALGO', type=int, default=2, help='0:SHA256/ 1:ECDSA_P-256/ 2:RSA-2048_SHA256')
+		parser.add_argument('-b', '--ROM_VER', type=int, default=1, help='Patchee\'s rom build version')
+		parser.add_argument('-j', '--ENTRY_ADDR', type=str, default='0x00023398', help='The big-endian address of the application\'s entry')
+		parser.add_argument('-p', '--PATCH_VER', type=int, default=2, help='Patchee\'s patch build version')
+		parser.add_argument('-r', '--IMG_TYPE', type=int, default=0, help='Patch Image type')
+		parser.add_argument('-s', '--ANTI_ROLLBACK_VER', type=int, default=3, help='Image anti-rollback version')
+		parser.add_argument('-t', '--PROD_ID', type=int, default=13, help='Specify production Types/ID')
+		parser.add_argument('-x', '--SERIAL_LOW', type=int, default=0, help='Minor serial number')
+		parser.add_argument('-y', '--SERIAL_HIGH', type=int, default=0, help='Major serial number')
 		args = parser.parse_args()
+
 		SIGN_ALGO = args.ALGO
 		IMG_TYPE = args.IMG_TYPE
 		PRODUCT_ID = args.PROD_ID
@@ -102,17 +108,37 @@ def optParser():
 		from optparse import OptionParser
 		print "using OptionParser"
 		parser = OptionParser(description=desc)
-		parser.add_option('-a', '--ALGO')
-		parser.add_option('-b', '--ROM_VER', default=0, help='Patchee\'s rom build version')
-		parser.add_option('-j', '--ENTRY_ADDR', default=0, help='The address of the application\'s entry')
-		parser.add_option('-p', '--PATCH_VER', default=0, help='Patchee\'s patch build version')
-		parser.add_option('-r', '--IMG_TYPE', default=0, help='Patch Image type')
-		parser.add_option('-s', '--ANTI_ROLLBACK_VER', default=0, help='Image anti-rollback version')
-		parser.add_option('-t', '--PROD_ID', default=0, help='Specify production Types/ID')
-		parser.add_option('-x', '--SERIAL_LOW', default=0, help='Minor serial number')
-		parser.add_option('-y', '--SERIAL_HIGH', default=0, help='Major serial number')
+		parser.add_option('-a', '--ALGO', type='int', default=2, help='0:SHA256/ 1:ECDSA_P-256/ 2:RSA-2048_SHA256')
+		parser.add_option('-b', '--ROM_VER', type=int, default=1, help='Patchee\'s rom build version')
+		parser.add_option('-j', '--ENTRY_ADDR', default='0x00023398', help='The big-endian address of the application\'s entry')
+		parser.add_option('-p', '--PATCH_VER', type=int, default=2, help='Patchee\'s patch build version')
+		parser.add_option('-r', '--IMG_TYPE', type=int, default=0, help='Patch Image type')
+		parser.add_option('-s', '--ANTI_ROLLBACK_VER', type=int, default=3, help='Image anti-rollback version')
+		parser.add_option('-t', '--PROD_ID', type=int, default=13, help='Specify production Types/ID')
+		parser.add_option('-x', '--SERIAL_LOW', type=int, default=0, help='Minor serial number')
+		parser.add_option('-y', '--SERIAL_HIGH', type=int, default=0, help='Major serial number')
 		(opt, arg) = parser.parse_args()
-	exit()
+
+		SIGN_ALGO = opt.ALGO
+		IMG_TYPE = opt.IMG_TYPE
+		PRODUCT_ID = opt.PROD_ID
+		ROM_BUILD_VER = opt.ROM_VER
+		PATCH_VER = opt.PATCH_VER
+		ANT_RB_VER = opt.ANTI_ROLLBACK_VER
+		SERIAL_LOW = opt.SERIAL_LOW
+		SERIAL_HIGH = opt.SERIAL_HIGH
+		ENTRY_ADDR = opt.ENTRY_ADDR
+	# ++debug++ #
+	print "signing algorithm: %d" %(SIGN_ALGO)
+	print "image type: %d" %(IMG_TYPE)
+	print "product id: %d" %(PRODUCT_ID)
+	print "rom build version: %d" %(ROM_BUILD_VER)
+	print "patch version: %d" %(PATCH_VER)
+	print "anti-rollback image version: %d" %(ANT_RB_VER)
+	print "serial low: %d" %(SERIAL_LOW)
+	print "serial high: %d" %(SERIAL_HIGH)
+	print "application entry address: %s" %(ENTRY_ADDR)
+	# --debug-- #
 
 
 def getRSAData(fname):
@@ -168,8 +194,12 @@ def getSignature(bstring):
         signature = signer.sign(hash)
 	return signature
 
+def TLVGenerator(input_list, output_fname):
+	pass
+	global TLV_LEN, TOTAL_LEN, ENTRY_ADDR, PATCH_LEN, PUB_KEY, CRC32, SIGNATURE
+
 def main():
-	global TLV_LEN, TOTAL_LEN, PATCH_LEN, PUB_KEY, CRC32, SIGNATURE
+	global TLV_LEN, TOTAL_LEN, ENTRY_ADDR, PATCH_LEN, PUB_KEY, CRC32, SIGNATURE
 	try:
 		with open(BIN_FIN, "r+b") as fin, open("test3", "w+b") as fout:
 			rampatch = fin.read()
@@ -200,19 +230,36 @@ def main():
 			fout_buf += struct.pack('B', ((TLV_RSP_CFG_ACK_CC_ACK_VSE) % 256)) # byte14
 			fout_buf += struct.pack('B', ((IMG_TYPE) % 256)) # byte15
 
-			fout_buf += PRODUCT_ID # byte16, 17
-			fout_buf += struct.pack('B', ((ROM_BUILD_VER >> 8) % 256)) # byte18
-			fout_buf += struct.pack('B', ((ROM_BUILD_VER) % 256)) # byte19
+			fout_buf += struct.pack('B', ((PRODUCT_ID) % 256)) # byte16
+			fout_buf += struct.pack('B', ((PRODUCT_ID >> 8)) % 256) # byte17
+			fout_buf += struct.pack('B', ((ROM_BUILD_VER >> 8) % 256)) # byte18 ?
+			fout_buf += struct.pack('B', ((ROM_BUILD_VER) % 256)) # byte19 ?
 			
-			fout_buf += PATCH_VER # byte20, 21
+			fout_buf += struct.pack('B', ((PATCH_VER) % 256)) # byte20
+			fout_buf += struct.pack('B', ((PATCH_VER >> 8) % 256)) # byte21
 			fout_buf += RSV_BYTES_0 # byte22, 23
 
-			fout_buf += binascii.a2b_hex(ANT_RB_VER) # byte24-27
-			fout_buf += binascii.a2b_hex(SERIAL_LOW) # byte28-31
-			fout_buf += binascii.a2b_hex(SERIAL_HIGH) # byte32, 33
+			fout_buf += struct.pack('B', ((ANT_RB_VER) % 256)) # byte24
+			fout_buf += struct.pack('B', ((ANT_RB_VER >> 8) % 256))# byte25
+			fout_buf += struct.pack('B', ((ANT_RB_VER >> 16) % 256))# byte26
+			fout_buf += struct.pack('B', ((ANT_RB_VER >> 24) % 256))# byte27
+			fout_buf += struct.pack('B', ((SERIAL_LOW) % 256)) # byte28
+			fout_buf += struct.pack('B', ((SERIAL_LOW >> 8) % 256)) # byte29
+			fout_buf += struct.pack('B', ((SERIAL_LOW >> 16) % 256)) # byte30
+			fout_buf += struct.pack('B', ((SERIAL_LOW >> 24) % 256)) # byte31
+			fout_buf += struct.pack('B', ((SERIAL_HIGH) % 256)) # byte32
+			fout_buf += struct.pack('B', ((SERIAL_HIGH >> 8) % 256)) # byte33
 
 			fout_buf += RSV_BYTES_1 # byte34, 35
-			fout_buf += ENTRY_ADDR
+			
+			print '*'
+			print 'address: %s' %(ENTRY_ADDR)
+			print '*'
+			ENTRY_ADDR = ENTRY_ADDR.lstrip('0x').zfill(8)
+			fout_buf += binascii.a2b_hex(ENTRY_ADDR[6:])
+			fout_buf += binascii.a2b_hex(ENTRY_ADDR[4:6])
+			fout_buf += binascii.a2b_hex(ENTRY_ADDR[2:4])
+			fout_buf += binascii.a2b_hex(ENTRY_ADDR[0:2])
 			# header finished #
 
 			fout_buf += rampatch
